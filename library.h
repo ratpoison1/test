cb371db2e70ecc17fcb780f7a8287436b09ab30d
yunkuk <dformouse@naver.com>
2018-11-18 23:11:10 +0900

    --need-- code_[num] addition time-out return

--- library.h
+++ library.h
@@ -1,10 +1,13 @@
 #include "resource.h"
 #include "member.h"
 #include "input.h"
+#include "space.h"
 #include <string>
 #include <sstream>
 #include <fstream>
 #define MAX 500
+#define SEAT 3
+#define STUDYROOM 10
 using namespace std;
 
 class library{
@@ -12,13 +15,18 @@ class library{
 		int num_input;
 		int num_book;
 		int num_undergraduate;
+		int num_empty;
 		book* book_p[MAX];
 		undergraduate* undergraduate_p[MAX];
+		seat* seat_p[SEAT];
+		studyroom* studyroom_p[STUDYROOM];
 		string input_list[MAX];
+		string empty_list[MAX];
 	public:
 		library();
 		void init();
 		void do_work();
+		void refresh(string date, string past_date, string space_type);
 		book* find_book(string name);
 		undergraduate* find_undergraduate(string name);
 		void code_0(ofstream &fp, string resource_name, string member_name, char type, string date);
@@ -29,12 +37,22 @@ class library{
 		void code_5(ofstream &fp, string return_date);
 		void code_6(ofstream &fp, string restricted_date);
 		void code_7(ofstream &fp, string resource_name, string member_name, string date, int delay);
-		int get_sum(string date);
+		void code_8();
+		void code_9();
+		void code_10();
+		void code_11();
+		void code_12();
+		void code_13();
+		void code_14();
+		float get_sum(string date);
+		void sort_input();
 };
 
 library :: library(){
 	num_undergraduate = 0;
+	num_empty = 0;
 	init();
+	sort_input();
 	do_work();
 }
 
@@ -59,6 +77,8 @@ void library :: init(){
 	char temp[MAX];
 	undergraduate* u;
 	book* b;
+	seat* st;
+	studyroom* sr;
 	int i = 0;
 	string tok;
 	num_input = 0;
@@ -80,7 +100,7 @@ void library :: init(){
 	fp.close();
 
 	i = 0;
-	ifstream fpi("input.dat");
+	ifstream fpi("input.dat"); // save input.dat
 	fpi.getline(temp, MAX);
 	while(1){
 		fpi.getline(temp, MAX);
@@ -104,7 +124,7 @@ void library :: init(){
 	}
 	fpi.close();
 
-/*	ifstream fpi2("space.dat");
+	ifstream fpi2("space.dat"); // save space.dat
 	fpi2.getline(temp, MAX);
 	while(1){
 		fpi2.getline(temp, MAX);
@@ -126,8 +146,17 @@ void library :: init(){
 			}
 		}
 	}
-	fpi2.close();*/
+	fpi2.close();
 	num_undergraduate = i;
+
+	for(i = 0; i < STUDYROOM; i++){
+		sr = new studyroom(i);
+		studyroom_p[i] = sr;
+	}
+	for(i = 0; i < SEAT; i++){
+		st = new seat(i);
+		seat_p[i] = st;
+	}
 }
 
 void library :: do_work(){
@@ -136,6 +165,8 @@ void library :: do_work(){
 	int yy, mm, dd, yy_d, mm_d, dd_d;
 	undergraduate* up;
 	book* bp;
+	seat* st;
+	studyroom* sr;
 
 	char operation;
 	string resource_type;
@@ -144,7 +175,16 @@ void library :: do_work(){
 	string member_name;
 	string date;
 
-//	char temp[MAX];
+	string space_type;
+	string time_string;
+	char space_number_char;
+	char number_of_member_char;
+	char time_char;
+	int space_number;
+	int number_of_member;
+	int time;
+	int past_date;
+
 	string temp;
 	string tok;
 	ofstream outf("output.dat");
@@ -160,10 +200,24 @@ void library :: do_work(){
 			stringstream ss(temp);
 			ss >> tok;
 			date = tok;
-			ss >> tok;
-			resource_type = tok;
-			ss >> tok;
-			resource_name = tok;
+			if(date[2] != '/'){
+				ss >> tok;
+				space_type = tok;
+				ss >> tok;
+				space_number_char = tok[0];
+				space_number = space_number_char-48;
+				if(space_type == "StudyRoom")
+					sr = studyroom_p[space_number-1];
+				else
+					st = seat_p[space_number-1];
+			}
+			else{
+				ss >> tok;
+				resource_type = tok;
+				ss >> tok;
+				resource_name = tok;
+				bp = find_book(resource_name);
+			}
 			ss >> tok;
 			operation = tok[0];
 			ss >> tok;
@@ -171,39 +225,118 @@ void library :: do_work(){
 			ss >> tok;
 			member_name = tok;
 
-			up = find_undergraduate(member_name);
-			bp = find_book(resource_name);
-
-			if(operation == 'B'){
-				if(!bp)
-					code_1(outf);
-				else if(up->get_borrowed())
-					code_2(outf);
-				else if(bp->get_state() == 'B'){
-					if(bp->get_who_borrowed() == member_name)
-						code_4(outf, up->get_borrowed_date());
-					else
-						code_5(outf, find_undergraduate(bp->get_who_borrowed())->get_deadline());
+			if(date[2] != '/' && operation == 'B'){
+				ss >> tok;
+				number_of_member_char = tok[0];
+				number_of_member = number_of_member_char-48;
+				ss >> tok;
+				if(tok.length() == 1){
+					time_char = tok[0];
+					time = time_char - 48;
 				}
-				else if(up->get_restricted())
-					if(get_sum(date) - get_sum(up->get_restricted_date()) <= 0)
-						code_6(outf, up->get_restricted_date());
+				else{
+					time_string = tok;
+					time = (time_string[0]-48)*10 + time_string[1]-48;
+					if(space_type == "Seat" && member_type == "Undergraduate"){
+						if(time > 3) time = 3;
+					if(space_type == "Studyroom" && member_type == "Undergraduate"){
+						if(time > 6) time = 6;
+					}
+				}
+			}
+			up = find_undergraduate(member_name);
+
+
+			if(date[2] != '/'){
+				if(operation == 'B'){
+					if(!bp) code_1(outf);
+					else if(up->get_borrowed()) code_2(outf);
+					else if(bp->get_state() == 'B'){
+						if(bp->get_who_borrowed() == member_name)
+							code_4(outf, up->get_borrowed_date());
+						else
+							code_5(outf, find_undergraduate(bp->get_who_borrowed())->get_deadline());
+					}
+					else if(up->get_restricted())
+						if(get_sum(date) - get_sum(up->get_restricted_date()) <= 0)
+							code_6(outf, up->get_restricted_date());
+						else
+							code_0(outf, resource_name, member_name, 'B', date);
 					else
 						code_0(outf, resource_name, member_name, 'B', date);
-				else
-					code_0(outf, resource_name, member_name, 'B', date);
+				}
+				else{
+					if(!up->get_borrowed()) code_3(outf);
+					else if(up->get_borrowed_book() != resource_name) code_3(outf);
+					else{
+						delay = get_sum(date) - get_sum(up->get_deadline());
+						if(delay > 0)	
+							code_7(outf, resource_name, member_name, date, delay);
+						else
+							code_0(outf, resource_name, member_name, 'R', date);
+					}
+				}
 			}
 			else{
-				if(!up->get_borrowed())
-					code_3(outf);
-				else if(up->get_borrowed_book() != resource_name)	
-					code_3(outf);
-				else{
-					delay = get_sum(date) - get_sum(up->get_deadline());
-					if(delay > 0)	
-						code_7(outf, resource_name, member_name, date, delay);
-					else
-						code_0(outf, resource_name, member_name, 'R', date);
+				refresh(date, past_date, space_type); // refresh	
+				past_date = date;
+				if(space_type == "StudyRoom"){
+					if(operation == 'B'){
+						if(space_number > STUDYROOM) code_8();
+						else if(time < 9 || time > 18) code_9();
+						else if(up->get_borrowed_studyroom() != 0) code_11();
+						else if(number_of_member > 6) code_12();
+						else if(time + 3 > 18) code_13();
+						else if(sr->borrowed_number == 50) code_14();
+						else code_0();
+					}
+					else if(operation == 'R'){
+						if(space_number > STUDYROOM) code_8();
+						else if(time < 9 || time > 18) code_9();
+						else if(up->get_borrowed_studyroom() == 0) code_10();
+						else code_0();	
+					}
+					else if(operation == 'E'){
+						if(space_number > STUDYROOM) code_8();
+						else if(time < 9 || time > 18) code_9();
+						else if(up->get_borrowed_studyroom() == 0) code_10();
+						else code_0();	
+					}
+					else{
+						if(space_number > STUDYROOM) code_8();
+						else if(time < 9 || time > 18) code_9();
+						else if(up->get_borrowed_studyroom() == 0) code_10();
+						else code_0();	
+					}
+				}
+				if(space_type == "Seat"){
+					if(operation == 'B'){
+						if(space_number > STUDYROOM) code_8();
+						else if(time < 9 || time > 18) code_9();
+						else if(up->get_borrowed_seat() != 0) code_11();
+						else if(number_of_member > 6) code_12();
+						else if(time + 3 > 18) code_13();
+						else if(sr->borrowed_number == 50) code_14();
+						else code_0();
+					}
+					else if(operation == 'R'){
+						if(space_number > STUDYROOM) code_8();
+						else if(time < 9 || time > 18) code_9();
+						else if(up->get_borrowed_studyroom() == 0) code_10();
+						else code_0();	
+					}
+					else if(operation == 'E'){
+						if(space_number > STUDYROOM) code_8();
+						else if(time < 9 || time > 18) code_9();
+						else if(up->get_borrowed_studyroom() == 0) code_10();
+						else code_0();	
+					}
+					else{
+						if(space_number > STUDYROOM) code_8();
+						else if(time < 9 || time > 18) code_9();
+						else if(up->get_borrowed_studyroom() == 0) code_10();
+						else code_0();	
+					}
 				}
 			}
 		}
@@ -313,12 +446,72 @@ void library :: code_7(ofstream &fp, string resource_name, string member_name, s
 	fp << "7\tDelayed return. You'll be restricted until " << restricted_date << endl;
 }
 
-int library :: get_sum(string date){
-	int yy, mm, dd;
-	yy = (date[0]-48)*10 + date[1]-48;
-	mm = (date[3]-48)*10 + date[4]-48;
-	dd = (date[6]-48)*10 + date[7]-48;
-
-	return yy*360 + mm*12 + dd;
+float library :: get_sum(string date){
+	int yy, mm, dd, hh;
+	if(date[2] == '/'){
+		yy = (date[0]-48)*10 + date[1]-48;
+		mm = (date[3]-48)*10 + date[4]-48;
+		dd = (date[6]-48)*10 + date[7]-48;
+		return yy*360 + mm*12 + dd;
+	}
+	else{
+		yy = (date[2]-48)*10 + date[3]-48;
+		mm = (date[5]-48)*10 + date[6]-48;
+		dd = (date[8]-48)*10 + date[9]-48;
+		hh = (data[11]-48)*10 + date[12]-48;
+		return yy*360 + mm*12 + dd + hh/100;
+	}
 }
 
+void library :: sort_input(){
+	int i;
+	string tok1, tok2, temp;
+	i = 0;
+	whlie(1){
+		stringstream ss1(input_list[i]);
+		ss1 >> tok1;
+		i++;
+		if(i == num_input break);
+		else{
+			stringstream ss2(input_list[i]);
+			ss2 >> tok2;
+		}
+		if(get_sum[tok1] > get_sum[tok2]){
+			temp = input_list[i-1];
+			input_list[i-1] = input_list[i];
+			input_list[i] = temp;
+			i = 0;
+		}
+	}
+}
+
+void library :: refresh(string date_r, string past_date_r){
+	int i, j;
+	undergraduate* up;
+	string date, past_date;
+	for(i = 0; i < 8; i++) date[i] = date_r[i+2];
+	for(i = 0; i < 8; i++) past_date[i] = past_date_r[i+2];
+
+	if(date == past_date){
+		for(i = 0; i < num_empty; i++){
+			up = find_undergraduate(empty_list[i]);
+			if(get_sum(date_r) > get_sum(up->get_empty_date())){
+				up->set_borrowed_seat = 0;	
+				for(j = 0; j < num_empty-1; j++){
+					empty_list[j] = empty_list[j+1];
+				}
+				num_empty--;
+			}
+		}
+	}
+	else{
+		num_empty = 0;
+		for(i = 0; i < num_undergraduate; i++){
+			up = undergraduate_p[i];
+			up->borrowed_studyroom = 0;
+			up->borrowed_seat = 0;
+		}
+		for(i = 0; i < SEAT; i++) seat_p[i]->borrowed_number = 0;
+		for(i = 0; i < STUDYROOM; i++) studyroom_p[i]->borrowed_number = 0;
+	}
+}
--- member.h
+++ member.h
@@ -10,21 +10,35 @@ class member{
 class undergraduate : public member{
 	private:
 		bool borrowed;
+		int borrowed_studyroom;
+		int borrowed_seat;
 		bool restricted;
 		string restricted_date;
 		string borrowed_book;
 		string deadline;
 		string borrowed_date;
+		string empty_date;
 	public:
 		undergraduate(string n){
 			name = n;
 			borrowed = 0;
+			borrowed_studyroom = 0;
+			borrowed_seat = 0;
 		}
 		bool get_borrowed(){
 			return borrowed;
 		}
 		bool get_restricted(){
 			return restricted;
+		}
+		int get_borrowed_studyroom(){
+			return borrowed_studyroom;
+		}
+		int get_borrowed_seat(){
+			return borrowed_seat;
+		}
+		string get_empty_date(){
+			return empty_date;
 		}
 		string get_restricted_date(){
 			return restricted_date;
@@ -39,6 +53,15 @@ class undergraduate : public member{
 			return borrowed_date;	
 		}
 
+		void set_empty_date(string s){
+			empty_date = s;
+		}
+		void set_borrowed_studyroom(int n){
+			borrowed_studyroom = n;
+		}
+		void set_borrowed_seat(int n){
+			borrowed_seat = n;
+		}
 		void set_borrowed(bool n){
 			borrowed = n;
 		}
